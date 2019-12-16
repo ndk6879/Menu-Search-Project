fa32d5d43c132240af092bd8b8149a2f723adc15
ndk6879 <ndk6879@naver.com>
2019-12-16 20:42:09 +0900

    second modification in settings.py

--- foodproject/foodproject/settings.py
+++ foodproject/foodproject/settings.py
@@ -89,7 +89,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         'USER': 'DonggyunNam',
-        'PASSWORD': '0921ndkwew',
+        'PASSWORD': 'Lp212121',
         'HOST': 'DonggyunNam$default',
     }
 }
