399dec5e93047fd23f41752011271f9771deeaa8
ndk6879 <ndk6879@naver.com>
2020-08-09 22:16:24 +0900

    back to previous in manage.py and wsgi.py

--- foodproject/foodproject/wsgi.py
+++ foodproject/foodproject/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'foodproject.settings.local')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'foodproject.settings')
 
 application = get_wsgi_application()
